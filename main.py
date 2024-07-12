0b1a02cdd77fe4e3ac4432c2000a9bda6941ddd5
Parsa-Hakimi <s.parsahakimi@gmail.com>
2024-07-13 00:04:41 +0330

    triangle calculator based on bid and ask

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from market_repo import MarketRepository
 if __name__ == '__main__':
     market_repo = MarketRepository()
     tc = TriangleCalculator()
+    market_repo.add_callback(MarketRepository.update_by_order_list)
     market_repo.add_callback(tc.calculate)
-    market_repo.run()
+    while True:
+        market_repo.run()
