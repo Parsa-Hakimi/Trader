60d90ab9fc8aeb7444f583068292a828f574f01b
Mohammadreza MontazeriShatoori <mohammadreza.montazeri@karnameh.ir>
2024-07-16 16:12:21 +0330

    Add authentication to Bitpin API

--- bitpin_proxy.py
+++ bitpin_proxy.py
@@ -1,20 +1,66 @@
+import os
 from typing import Literal
 
 import requests
 
 BITPIN_URL = 'https://api.bitpin.ir'
-BITPIN_API_KEY = '<KEY>'
-BITPIN_SECRET_KEY = '<KEY>'
+BITPIN_API_KEY = os.environ.get('BITPIN_API_KEY')
+BITPIN_SECRET_KEY = os.environ.get('BITPIN_SECRET_KEY')
 
 
-class BitPinProxy:
+class BitpinProxy:
     def __init__(self):
         self.base_url = BITPIN_URL
+        self.access_token = ''
+        self.refresh_token = ''
+
+    def _send_request(self, path, method='get', body=None, authenticated=False):
+        url = f'{self.base_url}{path}'
+
+        request_method = getattr(requests, method.lower())
+
+        headers = {}
+        if authenticated:
+            self._ensure_access()
+            headers['Authentication'] = f'Bearer {self.access_token}'
+
+        resp = request_method(url, json=body)
+        if resp.status_code in [401, 403]:  # TODO: test to see if Bitpin return correct status codes
+            self.refresh()
+            resp = request_method(url, json=body)
+
+        return resp
+
+    def _ensure_access(self):
+        if not self.access_token:
+            if self.refresh_token:
+                self.refresh()
+            else:
+                self.login()
+
+    def login(self):
+        resp_body = self._send_request('/v1/usr/api/login/', method='post', body={
+            'api_key': BITPIN_API_KEY,
+            'secret_key': BITPIN_SECRET_KEY,
+        }).json()
+
+        self.access_token = resp_body['access']
+        self.refresh_token = resp_body['refresh']
+
+    def refresh(self):
+        resp = self._send_request('/v1/usr/refresh_token/', method='post', body={
+            'refresh': self.refresh_token,
+        })
+
+        if resp.status_code >= 300:
+            self.login()
+        else:
+            self.access_token = resp.json()['access']
 
     def get_open_orders(self, market_id, order_type: Literal['buy', 'sell'] = 'buy'):
-        url_tmpl = f'{self.base_url}/v2/mth/actives/{market_id}/?type={order_type}'
+        url_tmpl = f'/v2/mth/actives/{market_id}/?type={order_type}'
 
-        return requests.get(url_tmpl).json()
+        return self._send_request(url_tmpl).json()
 
     def post_order(
             self,
@@ -27,7 +73,7 @@ class BitPinProxy:
         if mode != 'limit':
             raise NotImplementedError
 
-        url_tmpl = f'{self.base_url}/v1/odr/orders/'
+        url = '/v1/odr/orders/'
         payload = {
             'market': market_id,
             'amount1': base_amount,
@@ -41,7 +87,7 @@ class BitPinProxy:
             # 'price_limit_oco': 0,
         }
 
-        return requests.post(url_tmpl, json=payload).json()
+        return self._send_request(url, method='post', body=payload, authenticated=True).json()
 
 
-bitpin_proxy = BitPinProxy()
+bitpin_proxy = BitpinProxy()
